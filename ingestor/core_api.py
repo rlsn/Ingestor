c087f5705f71c3c7e1263e17c831da61ac033fda
rlsn <yumo1996@gmail.com>
2024-08-12 23:51:52 +0900

    Update core_api.py

--- ingestor/core_api.py
+++ ingestor/core_api.py
@@ -147,7 +147,7 @@ def remove(name, subset=None, partitions=[], force_remove=False):
                 print("[INFO] Deletion aborted")
                 return
 
-        shutil.rmtree(os.path.join(DATA_DIR, metadata[name]["path"]), ignore_errors=True)
+        shutil.rmtree(os.path.normpath(os.path.join(DATA_DIR, metadata[name]["path"])), ignore_errors=True)
 
         # update metadata
         for subset in metadata[name]['subsets']:
@@ -172,7 +172,7 @@ def remove(name, subset=None, partitions=[], force_remove=False):
                 print("[INFO] Deletion aborted")
                 return
         info = metadata[name]["subsets"][subset]
-        shutil.rmtree(os.path.join(DATA_DIR, info["path"]), ignore_errors=True)
+        shutil.rmtree(os.path.normpath(os.path.join(DATA_DIR, info["path"])), ignore_errors=True)
         # update metadata
         for part in info['partitions']:
             info['partitions'][part]['downloaded']=False
@@ -184,7 +184,7 @@ def remove(name, subset=None, partitions=[], force_remove=False):
     # remove partitions
     for part in partitions:
         info = metadata[name]["subsets"][subset]["partitions"][part]
-        shutil.rmtree(os.path.join(DATA_DIR, info["path"]), ignore_errors=True)
+        shutil.rmtree(os.path.normpath(os.path.join(DATA_DIR, info["path"])), ignore_errors=True)
         # update metadata
         info['downloaded']=False
         info['n_samples']=0
@@ -265,7 +265,7 @@ def get_filepaths(name, subset=None, partitions=None, download_if_missing=False)
         tbd_parts = partitions
 
     download(name, subset, tbd_parts)
-    filepaths = [os.path.join(DATA_DIR, data_info["partitions"][part]["path"]) for part in tbd_parts]
+    filepaths = [os.path.normpath(os.path.join(DATA_DIR, data_info["partitions"][part]["path"])) for part in tbd_parts]
     return filepaths
 
 def load_dataset(name:str, subset:str=None, partitions:list=None, download_if_missing:bool=False, **kwargs)->pd.DataFrame:
