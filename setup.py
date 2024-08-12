2d482c218104cb1cee3a90198fed993bce0c0cab
rlsn <yumo1996@gmail.com>
2024-08-13 01:14:45 +0900

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygestor',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/rlsn/Ingestor',
     author='Yumo Wang',
     author_email='yumo1996@gmail.com',
